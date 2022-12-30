f92c66b4a6978a92af020fe360ed257c439b5136
Jean Leite <jgusleite@gmail.com>
2022-12-30 18:17:08 -0300

    Testing

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\dev\rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\dev\rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\dev\rust\guessing_game\target\debug\guessing_game.exe: C:\dev\rust\guessing_game\src\main.rs
