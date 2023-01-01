03c083e3ed1c65902de592847c608dd690ffdd52
Jean Leite <jgusleite@gmail.com>
2023-01-01 11:02:07 -0300

    Update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("hello, world");
+    f();
+}
+
+fn f() {
+    let mut x: String = String::new();
+    io::stdin().read_line(&mut x).expect("Failed to read line");
+    println!("{x}");
 }
