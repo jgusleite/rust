2687d5994ff196a855c63fa523b9f372320745cc
Jean Leite <jgusleite@gmail.com>
2023-01-02 10:28:48 -0300

    Update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,13 +1,19 @@
-use std::io;
-
 fn main() {
-    println!("hello, world!");
-    println!("test");
-    f();
+    let a: i32 = 3;
+    let b: i32 = 1 + 3;
+    assert_eq!(a, b, "testing if {} == {}", a, b);
 }
 
-fn f() {
-    let mut x: String = String::new();
-    io::stdin().read_line(&mut x).expect("Failed to read line");
-    println!("{x}");
-}
+/*
+    Ownership Rules
+
+    - Each value in Rust has an *owner*.
+    - There can only be one owner at a time.
+    - When the owner goes out of scope, the value will be dropped.
+
+    String type
+
+    - The memory must be requested from the memory allocator at runtime.
+    - We need a way of returning this memory to the allocator when we're done
+      with our String.
+ */
