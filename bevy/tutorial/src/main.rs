819ab50d9cc6bc05878858e21157159c47487246
Jean Leite <jgusleite@gmail.com>
2023-01-03 19:41:09 -0300

    Update

--- bevy/tutorial/src/main.rs
+++ bevy/tutorial/src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+// use bevy_rapier2d::prelude::*;
+// use leafwing_input_manager::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .insert_resource(WindowDescriptor {
+        title: "Rolling Game".into(),
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins).run();
 }
