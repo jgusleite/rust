7a52677187ff31e1829ee6b880441e3c57ee9069
Jean Leite <jgusleite@gmail.com>
2023-01-14 13:40:37 -0300

    Update

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,15 +1,10 @@
-fn main() {
-
+fn print(s: &String) {
+    println!("{s}");
 }
 
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    s.len()
+fn main() {
+    let mut s = String::from("hello");
+    print(&s);
+    s.push_str(" world");
+    print(&s);
 }
